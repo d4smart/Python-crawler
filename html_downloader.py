eef5732cc84bb43673c61669b9178a94539b3061
d4smart <d4smart@foxmail.com>
2016-06-02 17:02:11 +0800

    whole version with parser bug

--- html_downloader.py
+++ html_downloader.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python
 # coding=utf-8
+import urllib2
 
 class HtmlDownloader(object):
     
-    def downloader(self):
-        pass
+    def download(self, url):
+        if url is None:
+            return None
+
+        response = urllib2.urlopen(url)
+
+        if response.getcode() != 200:
+            return 200
+        return response.read()
--- spider_main.py
+++ spider_main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # coding=utf-8
-from Crawler import url_manager, html_downloader, html_parser, html_outputer
+import url_manager, html_downloader, html_parser, html_outputer
 
 class SpiderMain(object):
     def __init__(self):
@@ -13,19 +13,16 @@ class SpiderMain(object):
         count = 1
         self.urls.add_new_url(root_url)
         while self.urls.has_new_url():
-            try:
-                new_url = self.urls.get_new_url()
-                print 'Craw %d : %s' % (count, new_url)
-                html_cont = self.downloader.download(new_url)
-                new_urls, new_data = self.parser.parse(new_url, html_cont)
-                self.urls.add_new_url(new_urls)
-                self.outputer.collect_data(new_data)
+            new_url = self.urls.get_new_url()
+            print 'Craw %d : %s' % (count, new_url)
+            html_cont = self.downloader.download(new_url)
+            new_urls, new_data = self.parser.parse(new_url, html_cont)
+            self.urls.add_new_url(new_urls)
+            self.outputer.collect_data(new_data)
 
-                if count == 1000:
-                    break
-                count = count + 1
-            except:
-                print 'Craw failed'
+            if count == 1000:
+                break
+            count = count + 1
 
         self.outputer.output_html()
 
